fbeb7c39e18556a598e502a56ee0ebaa3b36361f
rfsjim <james@jimmynet.net>
2025-06-12 20:46:15 +1000

    Thurs, 12 Jun 2025 8:45PM +10:00

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,23 @@
 /**
+ * Proxy Pattern
+ * A Structural Pattern
+ * Used to control access to an object (security, lazy loading etc.)
+ * The software equivalent of a bouncer, middleman, or stunt double. It’s not the real thing,
+ * but it stands in front of the real thing, controlling access to it.
+ * Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object.
+ * A proxy controls access to the original object, allowing you to perform something either before or after the request
+ * gets through to the original object.
  * standIn Patterns
  * Proxies give more control over accesing an object.
  * Using the stand in allows you to perform other work in input and output prior to getting to the object
-*/
+ * Uses:
+ * Access Control (Protection Proxy),
+ * Lazy Initialization (Virtual Proxy),
+ * Remote Proxy,
+ * Caching Proxy,
+ * Logging / Monitoring Proxy,
+ * Smart Reference Proxy,
+ */
 
 /**
  * The Target interface shows what is in common between the actual object and the standIn
--- src/singleton.ts
+++ src/singleton.ts
@@ -2,6 +2,16 @@
  * Singleton Patterns
  * Share a single global instance throughout the application
  * Singletons are created once and can be access globally
+ * Singleton – only one instance allowed (like Highlander, but boring)
+ * Sounds like something that occurs at high noon in the Wild West. Sir, this town ain’t big enough for the both of us.
+ * There’s only one of me… and that’s all this program’s ever gonna get.
+ * Make sure there’s only one instance of a class, and provide a global point of access to it.
+ * A single config manager
+ * A single logging service
+ * A single database connection pool
+ * System Clock or Timer
+ * Game Engine Manager / Audio Manager
+ * A single point of truth 
 */
 
 class Singleton {
